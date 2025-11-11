c0c67e5caab7206626130e850e42bb1c34b98853
nhan3-uri <hh5681@wayne.edu>
2025-11-10 19:39:05 -0500

    committing UI changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -406,20 +406,6 @@ const Navbar = () => {
                 </Link>
               </li>
               <li>
-                <Link
-                  to={`/classroom/${classroomId}/badges`}
-                  className={`flex items-center gap-2 ${hoverClass} ${
-                    location.pathname.startsWith(`/classroom/${classroomId}/badges`)
-                      ? 'text-green-500'
-                      : ''
-                  }`}
-                  title="Badges"
-                >
-                  <Crown size={18} />
-                  <span>Badges</span>
-                </Link>
-              </li>
-              <li>
                 <div className="tooltip tooltip-bottom" data-tip="Challenge">
                   <Link
                     to={`/classroom/${classroomId}/challenge`}
@@ -437,6 +423,20 @@ const Navbar = () => {
                 >
                   <Star size={18} />
                   <span>Feedback</span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={`/classroom/${classroomId}/badges`}
+                  className={`flex items-center gap-2 ${hoverClass} ${
+                    location.pathname.startsWith(`/classroom/${classroomId}/badges`)
+                      ? 'text-green-500'
+                      : ''
+                  }`}
+                  title="Badges"
+                >
+                  <Crown size={18} />
+                  <span>Badges</span>
                 </Link>
               </li>
             </>
